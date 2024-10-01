2ddcb5b73716cb6f93eee5068d6677cd1790f353
SilvioRojas13 <gamerthdflow@gmail.com>
2024-09-30 20:53:45 -0600

    Añadir clase anidada con sus propiedades

---
+++ Universidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceUni
+{
+    public class Universidad
+    {
+        // Añadir clase anidada de estudiante
+        public class Estudiante
+        {
+            // Propiedades de cada estudiante
+            public string Nombre { get; set; }
+            public int TotalSesiones { get; set; }
+            public int SesionesAsistidas { get; set; }
+
+            // Constructor para inicializar al estudiante
+            public Estudiante(string nombre, int totalSesiones, int sesionesAsistidas)
+            {
+                Nombre = nombre;
+                TotalSesiones = totalSesiones;
+                SesionesAsistidas = sesionesAsistidas;
+            }
+        }
+    }
+}
